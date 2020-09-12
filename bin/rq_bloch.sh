f12975d43b13127afdb73eaf86c7da249615dbad
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-12 23:33:38 +0200

    Update rq_bloch.sh

--- bin/rq_bloch.sh
+++ bin/rq_bloch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 echo; echo; echo "Bloch Sphere Demo"
+
+export DISPLAY=:0
 cd ~
 
 if [ ! -d rasqberry-grok-bloch ]; then
@@ -9,10 +11,19 @@ fi
 
 
 cd rasqberry-grok-bloch/
-export DISPLAY=:0
+if [  ! -f rasqberry-bloch-isrunning ]; then
+   nohup python3 -m http.server 8000 &
+   echo $! > rasqberry-bloch-isrunning
+   nohup chromium-browser --start-fullscreen --no-sandbox --enable-webgl --ignore-gpu-blacklist --test-type  http://127.0.0.1:8000 &
+   echo $! >> rasqberry-bloch-isrunning
+else
+   kill `cat rasqberry-bloch-isrunning`
+   rm rasqberry-bloch-isrunning
+fi
 
-python3 -m http.server 8000 &
 
-chromium-browser --start-fullscreen --no-sandbox --enable-webgl --ignore-gpu-blacklist  http://127.0.0.1:8000 &
 
+
+
+killall python3 /usr/lib/chromium-browser/chromium-browser-v7 
 
