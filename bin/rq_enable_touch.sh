71bad9c93716ce524b5e49f94cd13410b99c71f6
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-19 21:11:43 +0200

    display

--- bin/rq_enable_touch.sh
+++ bin/rq_enable_touch.sh
@@ -3,4 +3,4 @@
 cd ~/
 [ -d Tools ] || mkdir Tools
 cd Tools
-[ -d LCD-show ] && ( cd LCD-show/ && sudo ./rotate.sh 180 ) || ( git clone https://github.com/goodtft/LCD-show && cd LCD-show/ && sudo ./MPI4008-show 180 )
+[ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && cd LCD-show/ && sudo ./MPI4008-show 180 )
