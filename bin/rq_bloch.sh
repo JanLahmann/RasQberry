1ade6a0f74447275e0159b858dfe329fba033ab4
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-20 22:41:34 +0200

    touch

--- bin/rq_bloch.sh
+++ bin/rq_bloch.sh
@@ -15,7 +15,7 @@ if [  ! -f rasqberry-bloch-isrunning ]; then
    nohup python3 -m http.server 8000 &
    echo $! > rasqberry-bloch-isrunning
 #   nohup chromium-browser --start-fullscreen --no-sandbox --enable-webgl --ignore-gpu-blacklist --test-type  http://127.0.0.1:8000 &
-nohup chromium-browser --start-fullscreen --enable-webgl --ignore-gpu-blacklist  http://127.0.0.1:8000 &
+   nohup chromium-browser --start-fullscreen --enable-webgl --ignore-gpu-blacklist  http://127.0.0.1:8000 &
    echo $! >> rasqberry-bloch-isrunning
 else
    kill -15 `cat rasqberry-bloch-isrunning`
--- bin/rq_enable_touch.sh
+++ bin/rq_enable_touch.sh
@@ -3,4 +3,4 @@
 cd ~/
 [ -d Tools ] || mkdir Tools
 cd Tools
-[ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && cd LCD-show/ && sudo ./MPI4008-show 180 )
+[ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && touch /home/pi/RasQberry/.is_enabled_touch4 && cd LCD-show/ && sudo ./MPI4008-show 180 )
