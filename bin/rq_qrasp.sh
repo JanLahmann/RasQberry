d5dc56e8f7a10e243fef32d57e8701b650258cc2
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-22 22:41:57 +0200

    qrasp

--- bin/rq_qrasp.sh
+++ bin/rq_qrasp.sh
@@ -3,11 +3,25 @@
 echo; echo; echo "Qrasp"
 source ~/rasqberry/bin/activate
 cd ~
-git clone https://github.com/ordmoj/qrasp
-cd ~/qrasp
-echo; echo; echo "set IBM Q Experience token"
-( echo "from getpass import getpass"; 
-  echo "token = getpass('Enter your IBM Q Experience Token: ')";
-  echo "print ('APItoken = \'' + str(token) + '\'')" 
- ) | python > Qconfig_IBMQ_experience.py
-#python main_controller.py
+
+if [ ! -d qrasp ]; then
+  git clone https://github.com/ordmoj/qrasp;
+
+  cd ~/qrasp
+  echo; echo; echo "set IBM Q Experience token"
+  ( echo "from getpass import getpass"; 
+    echo "token = getpass('Enter your IBM Q Experience Token: ')";
+    echo "print ('APItoken = \'' + str(token) + '\'')" 
+  ) | python > Qconfig_IBMQ_experience.py
+fi
+
+if [  ! -f qrasp-isrunning ]; then
+  whiptail --msgbox "Starting Qrasp Demo" 20 60 1
+  cd ~/qrasp
+  python main_controller.py
+  echo $! > qrasp-isrunning
+else
+   kill -15 `cat qrasp-isrunning`
+   rm qrasp-isrunning
+   /home/pi/rasqberry/bin/clear_sense.py
+fi
--- bin/rq_rasptie.sh
+++ bin/rq_rasptie.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #
 echo; echo; echo "Raspberry-Tie 5 Qubit Demo"
+source ~/rasqberry/bin/activate
 cd ~
 
 if [ ! -d quantum-raspberry-tie ]; then
-   git clone https://github.com/KPRoche/quantum-raspberry-tie;
+  git clone https://github.com/KPRoche/quantum-raspberry-tie;
 fi
 
 cd quantum-raspberry-tie
 
-source ~/rasqberry/bin/activate
-
 if [  ! -f raspberry-tie-isrunning ]; then
-  whiptail --msgbox "Atarting Rqapberry-Tie Demo" 20 60 1
+  whiptail --msgbox "Starting Rqapberry-Tie Demo" 20 60 1
   nohup python QuantumRaspberryTie.qiskit.py & # standard 5 Qubit example
   echo $! > raspberry-tie-isrunning
   #python QuantumRaspberryTie.qiskit.py expt16.qasm # 16 Qubit example
