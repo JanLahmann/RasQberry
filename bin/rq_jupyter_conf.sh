ac2112a8da99758b0d8c8c3613ec90f103b15512
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-22 21:42:29 +0200

    jupyter

--- bin/rq_jupyter_conf.sh
+++ bin/rq_jupyter_conf.sh
@@ -6,5 +6,5 @@ pip install --prefer-binary jupyter
 jupyter notebook --generate-config
 JUPYTER_PW=`(echo "from notebook.auth import passwd"; echo "print(passwd('RasQberry'))") | python`; echo $JUPYTER_PW
 JUPYTER_PW=$(echo ${JUPYTER_PW} | sed -e "s#/#\\\/#g")
-sed -i "s/#c.NotebookApp.password = ''/c.NotebookApp.password = '$JUPYTER_PW'/" ~/.jupyter/jupyter_notebook_config.py
-sed -i "s/#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '*'/" ~/.jupyter/jupyter_notebook_config.py
+sed -i "s/# c.NotebookApp.password = ''/c.NotebookApp.password = '$JUPYTER_PW'/" ~/.jupyter/jupyter_notebook_config.py
+sed -i "s/# c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '*'/" ~/.jupyter/jupyter_notebook_config.py
