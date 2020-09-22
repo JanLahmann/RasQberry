9bc2b25297321a7a33560e5829a45227062faeda
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-22 22:02:38 +0200

    rptie

--- bin/rq_rasptie.sh
+++ bin/rq_rasptie.sh
@@ -2,8 +2,13 @@
 #
 echo; echo; echo "Raspberry-Tie"
 cd ~
-git clone https://github.com/KPRoche/quantum-raspberry-tie
-#python QuantumRaspberryTie.qiskit.py # standard 5 Qubit example
+
+if [ ! -d quantum-raspberry-tie ]; then
+   git clone https://github.com/KPRoche/quantum-raspberry-tie;
+fi
+
+
+python QuantumRaspberryTie.qiskit.py # standard 5 Qubit example
 #python QuantumRaspberryTie.qiskit.py expt16.qasm # 16 Qubit example
 #/usr/bin/sense_emu_gui &
 #python QuantumRaspberryTie.qiskit.py -e  # "-e" forces to use the emulator
