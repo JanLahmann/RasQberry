66e7c20e5adc82138b1e5b62c56c132d0ad0198f
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-26 22:50:47 +0200

    desktop

--- bin/rq_bloch_desktop.sh
+++ bin/rq_bloch_desktop.sh
@@ -6,28 +6,24 @@ export DISPLAY=:0
 cd ~
 
 # check if GL2/GL3 driver is enabled
-[ ! -f /home/pi/RasQberry/.kms-gl-enabled ] && whiptail --msgbox "1. Bloch demo needs to be run once from rasqberry-config" 20 60 1 && return 1
+[ ! -f /home/pi/RasQberry/.kms-gl-enabled ] && whiptail --msgbox "GL driver not enabled. Bloch demo needs to be run once from rasqberry-config" 20 60 1 && return 1
 # check touchscreen calibration
-[ ! -f /home/pi/RasQberry/.is_tft_calibrated ] &&  whiptail --msgbox "2. Bloch demo needs to be run once from rasqberry-config" 20 60 1 && return 1
+[ ! -f /home/pi/RasQberry/.is_tft_calibrated ] &&  whiptail --msgbox "Touchscreen not calibrated. Bloch demo needs to be run once from rasqberry-config" 20 60 1 && return 1
+# check if Bloch demo is installed
+[ ! -d rasqberry-grok-bloch ] && whiptail --msgbox "Bloch demo code not downloaded. Bloch demo needs to be run once from rasqberry-config" 20 60 1 && return 1
 
-
-if [ ! -d rasqberry-grok-bloch ]; then
-whiptail --msgbox "git clone" 20 60 1
-   git clone https://github.com/JanLahmann/rasqberry-grok-bloch;
-fi
 
 
 cd rasqberry-grok-bloch/
-if [  ! -f rasqberry-bloch-isrunning ]; then
-whiptail --msgbox "before http" 20 60 1
-   nohup python3 -m http.server 8000 &
-   echo $! > rasqberry-bloch-isrunning
-whiptail --msgbox "before chrome" 20 60 1
-#   nohup chromium-browser --start-fullscreen --no-sandbox --enable-webgl --ignore-gpu-blacklist --test-type  http://127.0.0.1:8000 &
-   nohup chromium-browser --start-fullscreen --enable-webgl --ignore-gpu-blacklist  http://127.0.0.1:8000 &
-   echo $! >> rasqberry-bloch-isrunning
-else
-whiptail --msgbox "before kill" 20 60 1
+if [ -f rasqberry-bloch-isrunning ]; then
    kill -15 `cat rasqberry-bloch-isrunning`
    rm rasqberry-bloch-isrunning
 fi
+
+nohup python3 -m http.server 8000 &
+echo $! > rasqberry-bloch-isrunning
+#nohup chromium-browser --start-fullscreen --no-sandbox --enable-webgl --ignore-gpu-blacklist --test-type  http://127.0.0.1:8000 &
+setsid nohup chromium-browser --start-fullscreen --enable-webgl --ignore-gpu-blacklist  http://127.0.0.1:8000 &
+echo $! >> rasqberry-bloch-isrunning
+sleep 10
+#whiptail --msgbox "Please wait until BlochSphere Demo has started" 20 60 1
