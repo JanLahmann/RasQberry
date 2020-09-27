c6ea056567b7c472c685f918931d1dee36e27a9f
JanLahmann <jan.lahmann@de.ibm.com>
2020-09-27 12:30:47 +0200

    autostart bloch

--- bin/rq_enable_touch.sh
+++ bin/rq_enable_touch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
-cd ~/
-[ -d Tools ] || mkdir Tools
-cd Tools
-[ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && touch /home/pi/RasQberry/.is_enabled_touch4 && cd LCD-show/ && sudo ./MPI4008-show 180 )
+
+if [ -f /home/pi/RasQberry/.is_enabled_touch4 ]; then
+  return 0
+else
+  cd ~/
+  [ -d Tools ] || mkdir Tools
+  cd Tools
+  [ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && touch /home/pi/RasQberry/.is_enabled_touch4 && cd LCD-show/ && sudo ./MPI4008-show 180 )
+fi
