3a162b9b8a6d3be1edb5d894fd25ad706904feff
JanLahmann <jan.lahmann@de.ibm.com>
2020-10-03 18:53:38 +0200

    bug

--- bin/rq_LED-off.py
+++ bin/rq_LED-off.py
@@ -31,11 +31,7 @@ def colorWipe(strip, color, wait_ms=10):
 
 
 # Main program logic follows:
-if __name__ == '__main__':
-    # Process arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()
+
 
     # Create NeoPixel object with appropriate configuration.
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
