343169d38a5a9a4363e7751aa7c0157679c20b73
JanLahmann <jan.lahmann@de.ibm.com>
2020-11-08 00:16:13 +0100

    bug

--- bin/rq_qrasp.sh
+++ bin/rq_qrasp.sh
@@ -13,7 +13,6 @@ cd qrasp
 
 if [  ! -f qrasp-isrunning ]; then
   [ ! -f /home/pi/.rq_no_messages ] && whiptail --msgbox "Starting Qrasp Demo" 20 60 1
-  cd ~/qrasp
   nohup python main_controller.py &
   echo $! > qrasp-isrunning
 else
