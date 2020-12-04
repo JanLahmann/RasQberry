d644b1c724abb0a2527580a032f4a9d69ebb6062
JanLahmann <jan.lahmann@de.ibm.com>
2020-12-04 10:52:19 +0100

    update Q020 install with debian package for rust , cargo

--- bin/rq_install_Qiskit020.sh
+++ bin/rq_install_Qiskit020.sh
@@ -26,19 +26,11 @@ echo; echo; echo "Install prereqs (retworkx pyscf cython six)"; echo;
 echo; echo; echo "if /boot/custom-wheels/ exists, use pre-compiled wheels on SD card"; echo;
 [ -d /boot/custom-wheels/ ] && pip install --prefer-binary /boot/custom-wheels/*`uname -m`.whl
 
-# if retworkx is not installed, install Rust compiler
-echo; echo; echo "if retworkx is not installed, install Rust compiler"; echo;
-[ "$(pip list | grep retworkx | wc -l)" -eq 0 ] && 
-pip install --prefer-binary setuptools-rust &&
-curl -o get_rustup.sh -s https://sh.rustup.rs &&
-sh ./get_rustup.sh -y && 
-source ~/.cargo/env
-
 echo; echo; echo "install retworkx pyscf cython six"; echo;
 pip install --prefer-binary retworkx pyscf cython six==1.14.*
 
 
-# 4.  Install qiskit
+# 3.  Install qiskit
 
 echo; echo; echo "Install Qiskit"; echo;
 pip install --prefer-binary 'qiskit[visualization]==0.20.*'
