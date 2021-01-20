d56d4176d374a769631c2c5e8523dd98136e99a3
JanLahmann <jan.lahmann@de.ibm.com>
2021-01-20 17:17:58 +0100

    add instructions for IBM Q Experience token

--- bin/rq_q_token.sh
+++ bin/rq_q_token.sh
@@ -8,6 +8,8 @@ source ~/rasqberry/bin/activate
 echo; echo; echo "store IBM Q Experience access token"
 (echo "from qiskit import IBMQ"; 
  echo "from getpass import getpass"; 
+ echo "Get access to your IBM Q Experience token as decribed here:";
+ echo "https://quantum-computing.ibm.com/docs/manage/account/";
  echo "token = getpass('Enter your IBM Q Experience Token: ')"; 
  echo "print ('APItoken = \'' + str(token) + '\'')";
  echo "IBMQ.save_account(token)") | python > ~/.Qconfig_IBMQ_experience.py
