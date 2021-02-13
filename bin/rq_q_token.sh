73a9178ff730c6481021938a92d4acfa76df76bd
JanLahmann <jan.lahmann@de.ibm.com>
2021-02-13 17:45:48 +0100

    fix IQX token configuration

--- bin/rq_q_token.sh
+++ bin/rq_q_token.sh
@@ -5,11 +5,11 @@ source ~/rasqberry/bin/activate
 [ -f ~/.qiskit/qiskitrc ] && rm ~/.qiskit/qiskitrc
 [ -f ~/.Qconfig_IBMQ_experience.py ] && rm ~/.Qconfig_IBMQ_experience.py
 
-echo; echo; echo "store IBM Q Experience access token"
+echo; echo; echo "store IBM Q Experience access token";
+echo "Get access to your IBM Q Experience token as decribed here:";
+echo "https://quantum-computing.ibm.com/docs/manage/account/";
 (echo "from qiskit import IBMQ"; 
  echo "from getpass import getpass"; 
- echo "Get access to your IBM Q Experience token as decribed here:";
- echo "https://quantum-computing.ibm.com/docs/manage/account/";
  echo "token = getpass('Enter your IBM Q Experience Token: ')"; 
  echo "print ('APItoken = \'' + str(token) + '\'')";
  echo "IBMQ.save_account(token)") | python > ~/.Qconfig_IBMQ_experience.py
