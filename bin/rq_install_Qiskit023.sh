442dd5192e96212b69f5829453dc41ddc593265a
JanLahmann <jan.lahmann@de.ibm.com>
2021-03-13 21:57:55 +0100

    fix

--- bin/rq_install_Qiskit023.sh
+++ bin/rq_install_Qiskit023.sh
@@ -19,8 +19,8 @@ echo 'export PATH=/home/pi/.local/bin:$PATH';
 # 2. Install qiskit
 
 echo; echo; echo "Install Qiskit"; echo;
-pip install --prefer-binary 'qiskit[visualization]==0.23.*'
-pip list | grep qiskit
+pip3 install --prefer-binary 'qiskit[visualization]==0.23.*'
+pip3 list | grep qiskit
 
 echo; echo "start Qiskit install: " $STARTDATE &&
 echo "end   Qiskit install: " `date`
