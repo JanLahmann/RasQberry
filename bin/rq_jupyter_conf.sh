e0fea7fb71f525d6f26474958109508711474beb
JanLahmann <jan.lahmann@de.ibm.com>
2021-03-14 12:05:22 +0100

    fix

--- bin/rq_jupyter_conf.sh
+++ bin/rq_jupyter_conf.sh
@@ -3,7 +3,7 @@
 echo; echo; echo "configure jupyter notebooks"
 #source ~/rasqberry/bin/activate
 pip3 install --prefer-binary jupyter
-jupyter notebook --generate-config
+/home/pi/.local/bin/jupyter notebook --generate-config
 JUPYTER_PW=`(echo "from notebook.auth import passwd"; echo "print(passwd('RasQberry'))") | python3`; echo $JUPYTER_PW
 JUPYTER_PW=$(echo ${JUPYTER_PW} | sed -e "s#/#\\\/#g")
 sed -i "s/# c.NotebookApp.password = ''/c.NotebookApp.password = '$JUPYTER_PW'/" ~/.jupyter/jupyter_notebook_config.py
