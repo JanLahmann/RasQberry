cb86b0ab070dfabbdba7af12e884a62917f221d6
JanLahmann <jan.lahmann@de.ibm.com>
2021-03-14 10:45:00 +0100

    fixes

--- bin/rq_install_Qiskit020.sh
+++ bin/rq_install_Qiskit020.sh
@@ -4,8 +4,10 @@
 #
 
 export STARTDATE=`date`
-echo; echo; echo "Install Qiskit"; echo;
+echo; echo; echo "Install Qiskit 0.20"; echo;
+. /home/pi/.bashrc
 
+pip3 install -U numpy Pillow
 pip3 install --prefer-binary 'qiskit[visualization]==0.20.*'
 pip3 list | grep qiskit
 
--- bin/rq_install_Qiskit023.sh
+++ bin/rq_install_Qiskit023.sh
@@ -4,8 +4,10 @@
 #
 
 export STARTDATE=`date`
-echo; echo; echo "Install Qiskit"; echo;
+echo; echo; echo "Install Qiskit 0.23"; echo;
+. /home/pi/.bashrc
 
+pip3 install -U numpy Pillow
 pip3 install --prefer-binary 'qiskit[visualization]==0.23.*'
 pip3 list | grep qiskit
 
