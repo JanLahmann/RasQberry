8f6f8bc9e3d3fdba94125f9d19f1f20ab6a18176
JanLahmann <jan.lahmann@de.ibm.com>
2021-04-06 16:20:34 +0200

    fix aer in Qiskit 0.25

--- bin/rq_install_Qiskit025.sh
+++ bin/rq_install_Qiskit025.sh
@@ -10,7 +10,9 @@ LLVM_CONFIG=llvm-config-9 pip3 install llvmlite
 pip3 install --prefer-binary -U Pillow
 pip3 install --prefer-binary numpy==1.20.1 scipy==1.6.1 decorator==4.4.*
 pip3 install git+https://github.com/Qiskit/qiskit-terra.git@0.17.0
-pip3 install cmake
+#pip3 install cmake==3.18.2
+sudo pip3 install conan
+#pip3 install qiskit-aer
 #pip3 install git+https://github.com/Qiskit/qiskit-aer.git@0.8.0
 pip3 install --prefer-binary 'qiskit[visualization,all]==0.25.*'
 
