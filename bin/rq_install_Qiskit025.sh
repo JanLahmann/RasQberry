a803c22c83ad4e0b587db390c6afd3caddd34a1d
JanLahmann <jan.lahmann@de.ibm.com>
2021-04-06 07:27:32 +0200

    add Qiskit 0.24 & 0.25

---
+++ bin/rq_install_Qiskit025.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# installation of Qiskit 0.25
+#
+
+export STARTDATE=`date`
+echo; echo; echo "Install Qiskit 0.25"; echo;
+
+LLVM_CONFIG=llvm-config-9 pip3 install llvmlite
+pip3 install git+https://github.com/Qiskit/qiskit-terra.git@0.17.0
+pip3 install git+https://github.com/Qiskit/qiskit-aer.git@0.8.0
+pip3 install --prefer-binary 'qiskit[visualization,all]==0.25.*'
+
+pip3 list | grep qiskit
+
+echo; echo "start Qiskit install: " $STARTDATE &&
+echo "end   Qiskit install: " `date`
