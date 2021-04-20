04e764f25bc3f33e99efa15cf0fcef5f94c17947
JanLahmann <jan.lahmann@de.ibm.com>
2021-04-20 22:27:41 +0200

    merge updates from dev branch to master (#50)

--- bin/rq_install_Qiskit023.sh
+++ bin/rq_install_Qiskit023.sh
@@ -6,22 +6,9 @@
 export STARTDATE=`date`
 echo; echo; echo "Install Qiskit 0.23"; echo;
 
-<<<<<<< HEAD
-pip3 install --upgrade pip
-python3 -m pip install virtualenv
-python3 -m virtualenv rasqberry
-source ~/rasqberry/bin/activate
-
-# 2. Install qiskit
-
-echo; echo; echo "Install Qiskit"; echo;
-pip install --prefer-binary 'qiskit[visualization]==0.23.*'
-pip list | grep qiskit
-=======
 pip3 install -U numpy Pillow
 pip3 install --prefer-binary 'qiskit[visualization]==0.23.*'
 pip3 list | grep qiskit
->>>>>>> JRL-dev3
 
 echo; echo "start Qiskit install: " $STARTDATE &&
 echo "end   Qiskit install: " `date`
