88ba79b807ccc3c24abf9f437d88283a8317f3c7
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-08 13:48:35 +0200

    added menu option for HD Demos

--- bin/rq_fun_with_quantum.sh
+++ bin/rq_fun_with_quantum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Open Funk-with-Quantum Repo on GitHub
+# Open Fun-with-Quantum Repo on GitHub
 
 [ -f /home/pi/nohup.out ] && rm -f /home/pi/nohup.out
 setsid nohup chromium-browser --start-fullscreen --enable-webgl --ignore-gpu-blacklist https://github.com/JanLahmann/Fun-with-Quantum#fun-with-quantum &&
