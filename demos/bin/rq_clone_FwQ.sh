09449027ebcc34ccf8d8420d96d282cbf3999e5a
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-09 11:08:13 +0200

    commented cvxpy installation

--- demos/bin/rq_clone_FwQ.sh
+++ demos/bin/rq_clone_FwQ.sh
@@ -4,6 +4,6 @@
 
 cd ~/
 git clone https://github.com/JanLahmann/Fun-with-Quantum.git
-pip3 install --prefer-binary cvxpy
+#pip3 install --prefer-binary cvxpy
 export PATH=/home/pi/.local/bin:/home/pi/RasQberry/demos/bin:$PATH
 jupyter notebook Fun-with-Quantum $1 $2 $3
