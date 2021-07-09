38e800f02b82fadca10ccad85d1099820617b99a
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-09 12:18:04 +0200

    removed explicit PATH export

--- demos/bin/rq_clone_FwQ.sh
+++ demos/bin/rq_clone_FwQ.sh
@@ -5,5 +5,4 @@
 cd ~/
 git clone https://github.com/JanLahmann/Fun-with-Quantum.git
 #pip3 install --prefer-binary cvxpy
-export PATH=/home/pi/.local/bin:/home/pi/RasQberry/demos/bin:$PATH
 jupyter notebook Fun-with-Quantum $1 $2 $3
--- demos/bin/rq_qiskit_tutorial.sh
+++ demos/bin/rq_qiskit_tutorial.sh
@@ -6,5 +6,4 @@ echo; echo; echo "install Qiskit tutorials"
 cd ~/
 git clone https://github.com/Qiskit/qiskit-tutorials
 pip3 install --prefer-binary cvxpy
-export PATH=/home/pi/.local/bin:/home/pi/RasQberry/demos/bin:$PATH
 jupyter notebook qiskit-tutorials $1 $2 $3
