6051e5bf48fd44e2aefb1291e94388ec14c58e98
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-13 14:12:20 +0200

    moved commands to different file

--- demos/bin/rq_jupyter_widget.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# configurations for the ibm_quantum_widgets
-
-echo "Jupyter Notebook Widgets";
-
-pip3 install ipwidgets
-jupyter nbextension enable --py widgetsnbextension
-jupyter nbextension enable --py --user ibm_quantum_widgets
