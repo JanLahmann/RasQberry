1f85a81cc6500aad560f99ce9625ac728f8a6806
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-21 11:42:05 +0200

    updated menu, fixed automation and added file to clone repos

---
+++ demos/bin/rq_clone_repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# If not already cloned clone:
+# - Fun-with-Quantum
+# - Qiskit-tutorials
+#
+
+cd ~
+
+if [ ! -d Fun-with-Quantum ]; then
+   git clone https://github.com/JanLahmann/Fun-with-Quantum.git;
+   echo "Cloned Fun-with-Quantum Repository"
+fi
+
+cd ~ 
+
+if [ ! -d qiskit-tutorials ]; then
+   git clone https://github.com/Qiskit/qiskit-tutorials;
+   echo "Cloned Qiskit-tutorials Repository"
+fi
--- demos/bin/start_jupyter_notebooks.sh
+++ demos/bin/start_jupyter_notebooks.sh
@@ -4,5 +4,5 @@
 # on port 8888 and port 8889
 #
 
-sudo -u pi -i nohup jupyter notebook --no-browser --port=8888 &
-sudo -u pi -i nohup jupyter notebook --no-browser --port=8889 &
+sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_qiskit_tutorial.sh -port 8888 --no-browser &
+sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_clone_FwQ.sh -port 8889 --no-browser &
