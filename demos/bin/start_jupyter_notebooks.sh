1040043bc66dd89ba37b7ebf9a3a718674163ee5
Jas53 <75606643+Jas53@users.noreply.github.com>
2021-07-21 12:07:29 +0200

    jupyter notebook autostart (#83)
    * jupyter notebook autostart
    
    * updated menu, fixed automation and added file to clone repos
    
    Co-authored-by: Jasmin Noll <jasmin.noll@ibm.com>

---
+++ demos/bin/start_jupyter_notebooks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# start jupyter notebooks
+# on port 8888 and port 8889
+#
+
+sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_qiskit_tutorial.sh -port 8888 --no-browser &
+sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_clone_FwQ.sh -port 8889 --no-browser &
