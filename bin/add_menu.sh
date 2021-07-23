d972d2854269d5097e9dcc74dcec6d9c9c96520a
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-23 15:46:40 +0200

    fixed filenames

--- bin/add_menu.sh
+++ bin/add_menu.sh
@@ -8,13 +8,13 @@ sudo sed -i '/<Merge type="menus"\/>/a \\t\t<Menuname>Demos</Menuname>' ~/../../
 sudo sed -i '/<Merge type="menus"\/>/a \\t\t<Separator/>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
 
 # Add HD Demos Category
-sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t</Menu> <!-- End HD Demos -->' ~/../../etc/xdg/menus/lxde-pi-applications.men$
+sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t</Menu> <!-- End HD Demos -->' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t</Include>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t</And>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
-sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t\t<Category>HDDemos</Category>' ~/../../etc/xdg/menus/lxde-pi-application$
+sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t\t<Category>HDDemos</Category>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t<And>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Include>' ~/../../etc/xdg/menus/lxde-pi-applications.menu  
-sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Directory>hd_demos.directory</Directory>' ~/../../etc/xdg/menus/lxde-pi-ap$
+sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Directory>hd_demos.directory</Directory>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Name>HD Demos</Name>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t<Menu>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t<!--HD Demos -->' ~/../../etc/xdg/menus/lxde-pi-applications.menu
@@ -24,10 +24,10 @@ sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t ' ~/../../etc/xdg/menus/lxde-pi-
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t</Menu> <!-- End Demos -->' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t</Include>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t</And>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
-sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t\t<Category>Demos</Category>' ~/../../etc/xdg/menus/lxde-pi-applications.$
+sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t\t<Category>Demos</Category>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t\t<And>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Include>' ~/../../etc/xdg/menus/lxde-pi-applications.menu  
-sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Directory>demos.directory</Directory>' ~/../../etc/xdg/menus/lxde-pi-appli$
+sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Directory>demos.directory</Directory>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t\t<Name>Demos</Name>' ~/../../etc/xdg/menus/lxde-pi-applications.menu
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t\t<Menu>' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
 sudo sed -i '/<\/Menu> <!-- End Other -->/a \\t<!--Demos -->' ~/../../etc/xdg/menus/lxde-pi-applications.menu 
