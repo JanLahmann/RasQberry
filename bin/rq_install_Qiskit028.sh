373912b509fc06d7befecc2281a3b74bb5a665c1
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-28 16:06:10 +0200

    Qiskit version 0.28 - first try

---
+++ bin/rq_install_Qiskit028.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# installation of Qiskit 0.28
+#
+
+export STARTDATE=`date`
+echo; echo; echo "Install Qiskit 0.28"; echo;
+
+pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.28.*'
+pip3 install --prefer-binary ibm-quantum-widgets
+
+pip3 list | grep qiskit
+
+echo; echo "start Qiskit install: " $STARTDATE &&
+echo "end   Qiskit install: " `date`
