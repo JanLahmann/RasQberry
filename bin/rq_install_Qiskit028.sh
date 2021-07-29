8533ef43423cc71f720fbe6699963816efe8c32c
Jasmin Noll <jasmin.noll@ibm.com>
2021-07-29 11:27:12 +0200

    Qiskit version 0.28 - second try

--- bin/rq_install_Qiskit028.sh
+++ bin/rq_install_Qiskit028.sh
@@ -6,6 +6,8 @@
 export STARTDATE=`date`
 echo; echo; echo "Install Qiskit 0.28"; echo;
 
+
+LLVM_CONFIG=llvm-config-9 pip3 install llvmlite
 pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.28.*'
 pip3 install --prefer-binary ibm-quantum-widgets
 
