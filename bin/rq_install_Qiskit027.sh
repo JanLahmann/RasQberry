b164fab20d78abb11e99f8579f336114c52af001
Jas53 <75606643+Jas53@users.noreply.github.com>
2021-08-01 15:18:29 +0200

    Changed fix-installed packages in installation file for qiskit version 0.27 (#92)
    * typo fixes qiskit 0.27
    
    * Qiskit v0.28
    
    * added x-bit
    
    * changed fix-installed packages
    
    Co-authored-by: Jasmin Noll <jasmin.noll@ibm.com>

--- bin/rq_install_Qiskit027.sh
+++ bin/rq_install_Qiskit027.sh
@@ -9,7 +9,7 @@ echo; echo; echo "Install Qiskit 0.27"; echo;
 
 LLVM_CONFIG=llvm-config-9 pip3 install llvmlite
 pip3 install --prefer-binary -U Pillow
-pip3 install --prefer-binary numpy==1.20.1 scipy==1.6.0 decorator==4.4.* sympy==1.7.1 retworkx==0.8.0 jupyter_core==4.6.1 matplotlib==3.3 traitlets==5.0
+pip3 install --prefer-binary jupyter_core==4.6.1 traitlets==5.0
 pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.27.*'
 pip3 install --prefer-binary ibm-quantum-widgets
 
