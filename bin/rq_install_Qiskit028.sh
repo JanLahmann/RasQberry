1a4988d9cb1c43abe92005bd8dec089623e46347
JanLahmann <jan.lahmann@de.ibm.com>
2021-08-02 08:09:08 +0200

    simplified Qiskit 0.28 install procedure

--- bin/rq_install_Qiskit028.sh
+++ bin/rq_install_Qiskit028.sh
@@ -6,9 +6,7 @@
 export STARTDATE=`date`
 echo; echo; echo "Install Qiskit 0.28"; echo;
 
-pip3 install --upgrade pip
-pip3 install --no-warn-script-location --prefer-binary scikit-build
-pip3 install --no-use-pep517 tweedledum
+pip3 install --prefer-binary "importlib-metadata<4" "pillow>=6.2.0" "decorator<5,>=4.3"
 pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.28.*'
 pip3 install --prefer-binary ibm-quantum-widgets
 
