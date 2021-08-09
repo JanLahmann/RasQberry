3ec42e2c501b132782a08ef04a414dabc556ccbb
Jasmin Noll <jasmin.noll@ibm.com>
2021-08-09 13:55:21 +0200

    Overlay and Exit

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,14 +1,21 @@
 import kivy
+from ewmh import EWMH
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 from kivy.config import Config
 import os
 import subprocess
 
-Config.set('graphics', 'width', '795')
-Config.set('graphics', 'height', '445')
-Config.write()
+#Config.set('graphics', 'width', '795')
+#Config.set('graphics', 'height', '445')
+#Config.write()
+
+Window.size = (800, 480)
+Window.borderless = True
+ewmh = EWMH()
+win = None
 
 # Create Screens
 class RootScreen(ScreenManager):
@@ -32,9 +39,20 @@ class QiskitScreen(Screen):
 class InfoScreen(Screen):
     pass
 
+class SmallOverlay(Screen):
+    def on_pre_enter(self, *args):
+        ewmh.setMoveResizeWindow(win, 0, 800 - 100, 480 - 80, 80, 40)
+        ewmh.display.flush()
+
 class Main(App):
     def build(self):
         return RootScreen()
+
+    def on_start(self):
+        global win
+        win = ewmh.getActiveWindow()
+        ewmh.setWmState(win, 1, '_NET_WM_STATE_ABOVE')
+        ewmh.display.flush()
 
 # run
 if __name__ == "__main__":
