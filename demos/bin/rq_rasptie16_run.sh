f446c64b1d1097040910c56914f974ae50d8bbae
JanLahmann <jan.lahmann@de.ibm.com>
2021-08-14 23:54:06 +0200

    fiy

--- demos/bin/rq_rasptie16_run.sh
+++ demos/bin/rq_rasptie16_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 echo; echo; echo "Raspberry-Tie 16 Qubit Demo"
-#source ~/rasqberry/bin/activate
 cd ~/quantum-raspberry-tie
 
-nohup sh -c 'sleep 5 && python3 QuantumRaspberryTie.qiskit.py $1 -local expt16.qasm' &
+#nohup sh -c 'sleep 5 && python3 QuantumRaspberryTie.qiskit.py $1 -local expt16.qasm' &
+sleep 5
+python3 QuantumRaspberryTie.qiskit.py $1 -local expt16.qasm &
--- demos/bin/rq_rasptie_run.sh
+++ demos/bin/rq_rasptie_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 echo; echo; echo "Raspberry-Tie 5 Qubit Demo"
-#source ~/rasqberry/bin/activate
 cd ~/quantum-raspberry-tie
 
-nohup sh -c 'sleep 5 && python3 QuantumRaspberryTie.qiskit.py $1 -local' &
+#nohup sh -c 'sleep 5 && python3 QuantumRaspberryTie.qiskit.py $1 -local' &
+sleep 5
+python3 QuantumRaspberryTie.qiskit.py $1 -local &
