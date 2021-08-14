f3a79e810fa9a4730f412375a4ebb94d1fc9558a
JanLahmann <jan.lahmann@de.ibm.com>
2021-08-14 23:36:21 +0200

    fix

--- bin/rq_sense_menu_run.sh
+++ bin/rq_sense_menu_run.sh
@@ -3,4 +3,6 @@
 echo; echo; echo "rq_sense_menu_run.sh"
 cd /home/pi/.local/bin
 
-nohup sh -c 'sleep 5 && python3 rq_sense_menu.py' &
+#nohup sh -c 'sleep 5 && python3 rq_sense_menu.py' &
+sleep 5
+python3 rq_sense_menu.py &
