1c170e91330cb3419aff6351fa57af3ec1c19ca2
Jasmin Noll <jasmin.noll@ibm.com>
2021-08-17 15:50:33 +0200

    updated code

--- bin/rq_install_kivy.sh
+++ bin/rq_install_kivy.sh
@@ -4,10 +4,7 @@
 # and all needed packages for the kivy interface
 
 pip3 install kivy
-sudo apt-get install libsdl2-2.0-0 
-sudo apt-get install libsdl2-image-2.0-0 
-sudo apt-get install libsdl2-mixer-2.0-0 
-sudo apt-get install libsdl2-ttf-2.0-0
+sudo apt install libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0
 pip3 install ewmh
 
 echo "Installed Kivy"
