99f02aa792dfad91a8f48323bc8422f860da74fa
Jasmin Noll <jasmin.noll@ibm.com>
2021-08-19 11:14:22 +0200

    test something

--- demos/bin/start_jupyter_qiskit.sh
+++ demos/bin/start_jupyter_qiskit.sh
@@ -4,4 +4,5 @@
 # on port 8888 
 #
 
-sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_qiskit_tutorial.sh -port 8888 &
+nohup jupyter notebook qiskit-tutorials -port 8888 &
+#sudo -u pi -i nohup /home/pi/RasQberry/demos/bin/rq_qiskit_tutorial.sh -port 8888 &
