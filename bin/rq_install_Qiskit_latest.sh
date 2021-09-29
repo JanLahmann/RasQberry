1a9e9e15eadf4eb348c217679b3600dcbd698e28
JanLahmann <jan.lahmann@de.ibm.com>
2021-09-29 21:48:43 +0200

    updates for Qiskit 0.30

--- bin/rq_install_Qiskit_latest.sh
+++ bin/rq_install_Qiskit_latest.sh
@@ -6,7 +6,9 @@
 export STARTDATE=`date`
 echo; echo; echo "Install Qiskit (latest version)"; echo;
 
-pip3 install --prefer-binary  "pillow>=6.2.0" "decorator<5,>=4.3" "numpy<1.23.0,>=1.17.3"
+#pip3 install --prefer-binary  "pillow>=6.2.0" "decorator<5,>=4.3" "numpy<1.23.0,>=1.17.3" 
+pip3 install --prefer-binary cmake scipy 
+sudo pip3 install conan
 pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]'
 pip3 install --prefer-binary ibm-quantum-widgets
 
