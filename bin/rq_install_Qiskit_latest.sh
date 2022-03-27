0163fe1de88c146ee05347a022dec9e0494bd32b
JanLahmann <jan.lahmann@de.ibm.com>
2022-03-27 09:28:00 +0200

    update qiskit install procedure 0.34

--- bin/rq_install_Qiskit_latest.sh
+++ bin/rq_install_Qiskit_latest.sh
@@ -7,9 +7,9 @@ export STARTDATE=`date`
 echo; echo; echo "Install Qiskit (latest version)"; echo;
 
 # install current version of rust; needed for retworkx
- curl -o get_rustup.sh -s https://sh.rustup.rs
- sh ./get_rustup.sh -y
- source $HOME/.cargo/env
+ #curl -o get_rustup.sh -s https://sh.rustup.rs
+ #sh ./get_rustup.sh -y
+ #source $HOME/.cargo/env
 
  pip3 install --prefer-binary cmake "pillow>=6.2.0" "decorator<5,>=4.3" "numpy<1.23.0,>=1.17.3" 
  pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]'
