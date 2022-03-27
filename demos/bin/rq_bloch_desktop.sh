7b9538f2196302c6018577b7fd5485d38e778236
JanLahmann <jan.lahmann@de.ibm.com>
2022-03-27 12:42:00 +0200

    fix bloch start script to separate from 4'' display setup

--- demos/bin/rq_bloch_desktop.sh
+++ demos/bin/rq_bloch_desktop.sh
@@ -4,24 +4,11 @@ echo; echo; echo "Bloch Sphere Demo"
 
 cd ~
 
-# check if GL2/GL3 driver is enabled
-if [ ! -f /home/pi/RasQberry/.kms-gl-enabled ]; then
-   whiptail --msgbox "GL driver not enabled. Bloch demo needs to be run once from rasqberry-config" 20 60 1
-   exit 1
-fi
-
-# check touchscreen calibration
-if [ ! -f /home/pi/RasQberry/.is_tft_calibrated ]; then
-   whiptail --msgbox "Touchscreen not calibrated. Bloch demo needs to be run once from rasqberry-config" 20 60 1
-   exit 1
-fi
-
 # check if Bloch demo is installed
 if [ ! -d rasqberry-grok-bloch ]; then
    git clone https://github.com/JanLahmann/rasqberry-grok-bloch;
    whiptail --msgbox "Bloch demo code downloaded." 20 60 1
 fi
-
 
 
 cd rasqberry-grok-bloch/
