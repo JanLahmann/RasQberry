0209b73dff7c0515340b4f8dc23786c8992682c7
JanLahmann <jan.lahmann@de.ibm.com>
2022-04-10 21:01:33 +0200

    update Button actions

--- bin/Button-action.py
+++ bin/Button-action.py
@@ -2,22 +2,32 @@
 
 # add to /etc/rc.local with
 # /usr/bin/python3 /home/pi/.local/bin/Button-action.py
-#  
+# Botton 1: GPIO Button 16 / LED 12, RasQ-LED start / LED clear
+# Botton 2: GPIO Button 16 / LED 12, reboot / shutdown
+# fyi: GPIO Button 24 / LED 23  works
 
 from gpiozero import Button, LED
 from signal import pause
 from time import sleep
-#from subprocess import check_call
+from subprocess import check_call,Popen
 import os
 
-button1 = Button(21, hold_time=2)
-led1 = LED(20)
+button1 = Button(16, hold_time=2)
+led1 = LED(12)
+
+button2 = Button(24, hold_time=2)
+led2 = LED(23)
 
 def pressed1():
-    led1.on()
+    led2.on()
+    sleep(0.1)
+    led2.off()
+    sleep(0.1)
+    led2.on()
     print("pressed1")
     os.system("touch /home/pi/pressed1")
-    #check_call(['touch', '/home/pi/pressed1'])
+    #check_call(['python3', '/home/pi/RasQberry/demos/bin/RasQ-LED.py'])
+    Popen(["/usr/bin/python3", "/home/pi/RasQberry/demos/bin/RasQ-LED.py"])
 
 def released1():
     led1.off()
@@ -38,9 +48,48 @@ def held1():
     print("held1")
     os.system("touch /home/pi/held1")
     #check_call(['touch', '/home/pi/held1'])
+    Popen(["/usr/bin/python3", "/home/pi/.local/bin/rq_LED-off.py"])
+
+
+def pressed2():
+    led2.on()
+    sleep(0.1)
+    led2.off()
+    sleep(0.1)
+    led2.on()
+    print("pressed2")
+    os.system("touch /home/pi/pressed2")
+    #check_call(['touch', '/home/pi/pressed2'])
+    os.system("/usr/sbin/reboot")
+
+def released2():
+    led2.off()
+    print("released2")
+    os.system("touch /home/pi/released2")
+    #check_call(['touch', '/home/pi/released2'])
+
+def held2():
+    led2.off()
+    sleep(0.1)
+    led2.on()
+    sleep(0.1)
+    led2.off()
+    sleep(0.1)
+    led2.on()
+    sleep(0.1)
+    led2.off()
+    print("held2")
+    os.system("touch /home/pi/held2")
+    #check_call(['touch', '/home/pi/held2'])
+    os.system("/usr/sbin/shutdown")
+
 
 button1.when_pressed = pressed1
 button1.when_released = released1
 button1.when_held = held1
+
+button2.when_pressed = pressed2
+button2.when_released = released2
+button2.when_held = held2
 
 pause()
--- bin/rq_install_Qiskit036.sh
+++ bin/rq_install_Qiskit036.sh
@@ -12,7 +12,7 @@ sh ./get_rustup.sh -y
 source $HOME/.cargo/env
 
 pip3 install --prefer-binary cmake "pillow>=6.2.0" "decorator<5,>=4.3" "numpy<1.23.0,>=1.21.0" 
-pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.34.*'
+pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.36.*'
 pip3 install --prefer-binary ibm-quantum-widgets
 
 pip3 list | grep qiskit
