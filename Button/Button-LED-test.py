2c1bd5f4423dbf192c0aa42ca0c590980e31a843
JanLahmann <jan.lahmann@de.ibm.com>
2022-04-10 11:27:43 +0200

    adding Arcade Button; add Button-LED-test.py

---
+++ Button/Button-LED-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# 
+#
+
+# test with     python3 Button-LED-test-py
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+LED_PIN = 20
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(LED_PIN,GPIO.OUT)
+print ("LED on")
+GPIO.output(LED_PIN,GPIO.HIGH)
+sleep(2)
+print ("LED off")
+GPIO.output(20,GPIO.LOW)
