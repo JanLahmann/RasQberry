9af222709a2083309694ac7e259480d9500709f8
JanLahmann <jan.lahmann@de.ibm.com>
2022-04-10 16:59:26 +0200

    fix

--- bin/Button-action.py
+++ bin/Button-action.py
@@ -6,41 +6,41 @@
 
 from gpiozero import Button, LED
 from signal import pause
-#from time import sleep
+from time import sleep
 #from subprocess import check_call
 import os
 
 button1 = Button(21, hold_time=2)
 led1 = LED(20)
 
-def pressed1(led):
-    led.on()
+def pressed1():
+    led1.on()
     print("pressed1")
     os.system("touch /home/pi/pressed1")
     #check_call(['touch', '/home/pi/pressed1'])
 
-def released1(led):
-    led.off()
+def released1():
+    led1.off()
     print("released1")
     os.system("touch /home/pi/released1")
     #check_call(['touch', '/home/pi/released1'])
 
-def held1(led):
-    led.off()
+def held1():
+    led1.off()
     sleep(0.1)
-    led.on()
+    led1.on()
     sleep(0.1)
-    led.off()
+    led1.off()
     sleep(0.1)
-    led.on()
+    led1.on()
     sleep(0.1)
-    led.off()
+    led1.off()
     print("held1")
     os.system("touch /home/pi/held1")
     #check_call(['touch', '/home/pi/held1'])
 
-button1.when_pressed = pressed1(led1)
-button1.when_released = released1(led1)
-button1.when_held = held1(led1)
+button1.when_pressed = pressed1
+button1.when_released = released1
+button1.when_held = held1
 
 pause()
