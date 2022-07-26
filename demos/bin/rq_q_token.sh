06f202ab843f938591c41db1faec5e4777676ef9
Petzys <87223648+Petzys@users.noreply.github.com>
2022-07-26 13:48:48 +0200

    Implemented usage of env-file in python and in shell scripts, work in progress

--- demos/bin/rq_q_token.sh
+++ demos/bin/rq_q_token.sh
@@ -10,10 +10,12 @@ echo "Get access to your IBM Q Experience token as decribed here:";
 echo "https://quantum-computing.ibm.com/docs/manage/account/";
 echo "Please wait for the prompt to enter your token";
 
-(echo "from qiskit import IBMQ"; 
+token=$( (echo "from qiskit import IBMQ";
  echo "from getpass import getpass"; 
  echo "token = getpass('Enter your IBM Q Experience Token: ')"; 
- echo "print ('APItoken = \'' + str(token) + '\'')";
- echo "IBMQ.save_account(token)") | python3 > ~/.Qconfig_IBMQ_experience.py
+ echo "print ('IBM_Q_API_TOKEN=\"' + str(token) + '\"')";
+ echo "IBMQ.save_account(token)") | python3 )
+
+sed -i "s/IBM_Q_API_TOKEN=.*/$token/gm" /home/pi/RasQberry/environment.env
 
 [ -f ~/qrasp/Qconfig_IBMQ_experience.py ] && ~/RasQberry/demos/bin/rq_qrasp_token.sh
