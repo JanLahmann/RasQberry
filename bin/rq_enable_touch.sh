4080af86457316546dd266b49a4fa79a81f4bf8e
Petzys <87223648+Petzys@users.noreply.github.com>
2022-07-26 15:38:55 +0200

    Implemented usage of env-file in python and in shell scripts, work in progress

--- bin/rq_enable_touch.sh
+++ bin/rq_enable_touch.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #
 
-if [ -f /home/pi/RasQberry/.is_enabled_touch4 ]; then
+. /home/pi/RasQberry/env-config.sh
+update_environment_file () {
+  sed -i "s/^$1=.*/$1=$2/gm" /home/pi/RasQberry/environment.env
+  . /home/pi/RasQberry/env-config.sh
+}
+
+if [ "$IS_ENABLED_TOUCH4" = true ]; then
   return 0
 else
   sudo cp /home/pi/.local/bin/rq_dispsetup.sh /usr/share/dispsetup.sh
   cd ~/
   [ -d Tools ] || mkdir Tools
   cd Tools
-  [ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && touch /home/pi/RasQberry/.is_enabled_touch4 && cd LCD-show/ && sudo ./MPI4008-show 180 )
+  [ -d LCD-show ] && ( echo "Automatic display configuration not possible" ) || ( git clone https://github.com/goodtft/LCD-show && update_environment_file "IS_ENABLED_TOUCH4" "true" && cd LCD-show/ && sudo ./MPI4008-show 180 )
 fi
