6d6b883710bdde4adda7b974f9ab16f068851bb4
Petzys <87223648+Petzys@users.noreply.github.com>
2022-07-26 17:13:51 +0200

    Implemented usage of env-file in python and in shell scripts, work in progress Working on flexible path names

--- env-config.sh
+++ env-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Absolute path this script is in
-RASQ_PATH="$(dirname "$(readlink -f -- "$0")")/RasQberry"
+RASQ_PATH="$(dirname "$(readlink -e -- "$0")")/RasQberry"
 echo "$RASQ_PATH"
 
 export $(grep -v "^#" "/home/pi/RasQberry/environment.env" | xargs -d "\n")
