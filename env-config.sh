4f050145ddef8624d26b3787a71b4289d5fa2cfe
Petzys <87223648+Petzys@users.noreply.github.com>
2022-07-26 14:38:01 +0200

    Implemented usage of env-file in python and in shell scripts, work in progress

--- env-config.sh
+++ env-config.sh
@@ -1,8 +1,1 @@
 export $(grep -v "^#" "/home/pi/RasQberry/environment.env" | xargs -d "\n")
-
-update_environment_file () {
-  sed -i "s/^$1=.*/$1=$2/gm" /home/pi/RasQberry/environment.env
-  export $(grep -v "^#" "/home/pi/RasQberry/environment.env" | xargs -d "\n")
-}
-
-export -f update_environment_file
