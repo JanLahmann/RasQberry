6f009a8e12729bac84bfcdf96b3fbfcbd78cbc57
Petzys <87223648+Petzys@users.noreply.github.com>
2022-08-29 16:46:33 +0200

    Tests

--- config/setup.sh
+++ config/setup.sh
@@ -92,9 +92,7 @@ do_rq_enable_docker() {
 do_rq_configure_button() {
   # add Button-action.py to /etc/rc.local
   # enable reboot overlay in /boot/config.txt
-  echo $BUTTON_CONFIGURED
-  if [ $"BUTTON_CONFIGURED" = false ]; then
-    echo "Hallloo"
+  if [ "$BUTTON_CONFIGURED" = false ]; then
     sed -i 's/exit 0//' /etc/rc.local
     echo "/usr/bin/python3 /home/pi/.local/bin/Button-action.py\n\nexit 0" >> /etc/rc.local
     echo "\n# enable shutdown/reboot on GPIO 3; and LED power indicator on GPIO 4\ndtoverlay=gpio-shutdown,gpio_pin=3\ngpio=4=op,dh" >> /boot/config.txt
