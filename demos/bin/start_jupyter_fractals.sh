2cff72a4a903e57b590d06a9a1adf702f04ff92f
Petzys <87223648+Petzys@users.noreply.github.com>
2022-08-30 14:01:57 +0200

    WOP start_jupyter_fractals.sh

--- demos/bin/start_jupyter_fractals.sh
+++ demos/bin/start_jupyter_fractals.sh
@@ -10,4 +10,4 @@ pipenv run jupyter nbextension enable --py --sys-prefix appmode
 pipenv run jupyter serverextension enable --py --sys-prefix appmode
 
 # start jupyter notebook with appmode
-jupyter notebook Visualizing-Quantum-Computing-using-fractals/quantum-fractals-multiple-complex-numbers/QuantumFractals1qubit_sphere_fractals_animation_images.ipynb --port 8889 --ip=127.0.0.1
+jupyter notebook --port 8889 --ip=127.0.0.1
