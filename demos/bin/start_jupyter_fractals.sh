9a61da689aa4b35bfd446cd076c16990af4172bd
Petzys <87223648+Petzys@users.noreply.github.com>
2022-08-30 16:00:27 +0200

    WOP start_jupyter_fractals.sh

--- demos/bin/start_jupyter_fractals.sh
+++ demos/bin/start_jupyter_fractals.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-git clone -b raspberry https://github.com/Petzys/Visualizing-Quantum-Computing-using-fractals.git
+if [ ! -d Visualizing-Quantum-Computing-using-fractals ]; then
+   git clone -b raspberry https://github.com/Petzys/Visualizing-Quantum-Computing-using-fractals.git;
+   echo "Cloned Fractal Repository"
+fi
 
+############### DIDNT WORK ###############
 #pip install pipenv
 #pipenv install --skip-lock appmode
 #pipenv install jupyter
@@ -9,7 +13,7 @@ git clone -b raspberry https://github.com/Petzys/Visualizing-Quantum-Computing-u
 # install jupyter appmode
 #pipenv run jupyter nbextension enable --py --sys-prefix appmode
 #pipenv run jupyter serverextension enable --py --sys-prefix appmode
+############### DIDNT WORK ###############
 
 # start jupyter notebook
-nohup jupyter notebook --port 8889 --ip=127.0.0.1 --no-browser &
-nohup xdg-open http://127.0.0.1:8889/notebooks/Visualizing-Quantum-Computing-using-fractals/quantum-fractals-multiple-complex-numbers/QuantumFractals1qubit_sphere_fractals_animation_images.ipynb
+sudo -u pi -i nohup jupyter notebook Visualizing-Quantum-Computing-using-fractals --port 8889 --ip=127.0.0.1 &
