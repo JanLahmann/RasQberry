5649c4227783370c7fd89972c6e65a5010877827
Petzys <87223648+Petzys@users.noreply.github.com>
2022-09-13 11:29:26 +0200

    Initial config now ignores installed packages and doesn't overwrite

--- config/setup.sh
+++ config/setup.sh
@@ -40,7 +40,9 @@ do_rq_initial_config() {
     # install Qiskit (this has to be done before installing via requirements.txt)
     do_rasqberry_install_general 037 silent
     # install python requirements
-    pip install --ignore-installed -r /home/pi/RasQberry/requirements.txt
+    pip install --upgrade pip
+    export PIP_IGNORE_INSTALLED=0
+    pip install -r /home/pi/RasQberry/requirements.txt
     update_environment_file "INITIAL_CONFIG" "true"
     if [ "$INTERACTIVE" = true ]; then
         [ "$RQ_NO_MESSAGES" = false ] && whiptail --msgbox "initial config completed" 20 60 1
