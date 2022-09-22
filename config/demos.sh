a614af843c04fd5f2e3bf8cd70da157b964c1c11
Bengt Wegner <87223648+Petzys@users.noreply.github.com>
2022-09-22 15:21:35 +0200

    Fractals are now in new file

--- config/demos.sh
+++ config/demos.sh
@@ -18,17 +18,6 @@ do_rasqberry_run_bloch(){
 }
 
 # Run the fractal demo in browser
-do_rasqberry_run_fractals_off(){
-  # check if Initial Setup is done
-  if [ "$REQUIREMENTS_INSTALLED" = false ]; then
-    do_rasqberry_install_requirements
-  fi
-  sudo apt-get install chromium-chromedriver
-  cd /home/pi/RasQberry/demos/bin/fractal_files || exit
-  sudo -u pi -H -- sh -c '/usr/bin/python3 /home/pi/RasQberry/demos/bin/fractal_files/fractals.py'
-  cd || exit
-}
-
 do_rasqberry_run_fractals(){
   sudo -u pi -H -- sh -c /home/pi/RasQberry/demos/bin/fractals.sh
 }
