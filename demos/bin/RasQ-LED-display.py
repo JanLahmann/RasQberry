27d944e48a1e74c07606bd00b3f85807090ffd9f
JanLahmann <jan.lahmann@de.ibm.com>
2022-10-03 13:26:24 +0200

    fix LED invert

--- demos/bin/RasQ-LED-display.py
+++ demos/bin/RasQ-LED-display.py
@@ -18,7 +18,8 @@ LED_PIN = int(config["LED_PIN"])
 LED_FREQ_HZ = int(config["LED_FREQ_HZ"])  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = int(config["LED_DMA"])          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = int(config["LED_BRIGHTNESS"])  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = bool(config["LED_INVERT"])    # True to invert the signal (when using NPN transistor level shift)
+#LED_INVERT = bool(config["LED_INVERT"])    # True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = config["LED_INVERT"]    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = int(config["LED_CHANNEL"])       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
 
