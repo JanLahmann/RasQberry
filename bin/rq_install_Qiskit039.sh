849d61a57f33f1ea2c54f00586ccf8e1f1093986
JanLahmann <jan.lahmann@de.ibm.com>
2023-03-05 20:56:20 +0100

    force conan 1.x for qiskit 0.39

--- bin/rq_install_Qiskit039.sh
+++ bin/rq_install_Qiskit039.sh
@@ -11,6 +11,13 @@ curl -o get_rustup.sh -s https://sh.rustup.rs
 sh ./get_rustup.sh -y
 source $HOME/.cargo/env
 
+# workaround to force conan v1.x
+git clone https://github.com/Qiskit/qiskit-aer/ --branch 0.11.2
+cd qiskit-aer
+sed -i "s/conan>=1.40.0/conan<2.0.0/" pyproject.toml
+pip install .
+
+
 pip3 install --no-warn-script-location --prefer-binary 'qiskit[visualization,all]==0.39'
 pip3 install --prefer-binary ibm-quantum-widgets
 
